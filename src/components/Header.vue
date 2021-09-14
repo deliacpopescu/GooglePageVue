db84cfaf7a9fabc706a61dc340e367c59323d553
delia_popescu <delia.popescu@principal33.com>
2021-09-15 00:28:37 +0300

    add and style google apps  hidden div

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,10 +8,24 @@
         <a href="https://www.google.ro/imghp?hl=en&ogbl">Images</a>
       </div>
       <div class="links">
-        <button id="menu-btn">
+        <button id="menu-btn" v-on:click="isHidden=!isHidden">
           <img alt="menu button" src="./../assets/dots-menu.svg" />
         </button>
+         <div v-if="!isHidden" class="google-apps">
+                    <!-- <div class="d-flex justify-content-around mt-3 mb-3"> -->
+                        <img src="./../assets/calendar.png" alt="calendar"/>
+                        <img src="./../assets/drive.png" alt="drive"/>
+                        <img src="./../assets/gmail.png" alt="gmail"/>
+                    <!-- </div>   
+                    <div class="d-flex justify-content-around mt-3">      -->
+                        <img src="./../assets/gmap.png" alt="gmap"/>
+                        <img src="./../assets/photos.png" alt="photos"/>
+                        <img src="./../assets/youtube.png" alt="youtube"/>
+                    <!-- </div> -->
+                </div>
       </div>
+               
+
       <div class="links">
         <button id="email-btn">D</button>
       </div>
@@ -21,7 +35,12 @@
 
 <script>
 export default {
-  main: "Header"
+  main: "Header",
+  data(){
+      return{
+          isHidden:true
+      }
+      }
 };
 </script>
 
@@ -65,5 +84,29 @@ a:hover {
   border: none;
   border-radius: 50%;
   color: white;
+}
+
+.google-apps{
+    background: white;
+    -webkit-border-radius: 4px;
+    border-radius: 4px;
+    border:1px solid grey;
+    box-shadow: 2px 2px #888888;
+    top:60px;
+    margin-right: 20px;
+    padding: 4px 8px;
+    position: absolute;
+    height:200px;
+    width: 200px;
+    right:0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.google-apps img{
+    width:50px;
+    height:50px;
+    margin:7px;
 }
 </style>
